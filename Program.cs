12b672201378f57a28cc6d41936c54ed6c6042b7
Thilo Viereck <profix898@users.noreply.github.com>
2023-09-12 10:35:51 +0200

    - Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Maui.Storage;
+
+namespace EssentialsApp1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine($"AppData folder: {FileSystem.AppDataDirectory}");
+    }
+}
